47c98cd4d6e15c60facb4e64d183c9377ac78053
Tuğrul Bayrak <tugrulbyrk1@gmail.com>
2017-05-01 16:49:33 +0300

    Fixed some permission problems.

--- app/src/main/java/com/example/tugrulbayrak/fennerv1/MainActivity.java
+++ app/src/main/java/com/example/tugrulbayrak/fennerv1/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
